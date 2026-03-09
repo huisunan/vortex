3f3e474c9a90175e74951ad7e074e9554d182be0
huisunan <571101320@qq.com>
2026-03-09 21:17:03 +0800

    test(back): run health endpoint test with test profile

--- back/src/test/java/com/vortex/health/HealthControllerTest.java
+++ back/src/test/java/com/vortex/health/HealthControllerTest.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.boot.health.actuate.endpoint.HealthEndpoint;
@@ -16,6 +17,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class HealthControllerTest {
 
     @Autowired
